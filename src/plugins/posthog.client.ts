35d2c6a4b61b22a267a13edb6bf0be09dfe87975
Julien Reichel <julien.reichel@bluewin.ch>
2026-01-24 16:47:55 +0100

    fix: manually track SSR page views

--- src/plugins/posthog.client.ts
+++ src/plugins/posthog.client.ts
@@ -9,9 +9,26 @@ export default defineNuxtPlugin((_nuxtApp) => {
 
   const posthogClient = posthog.init(cfg.posthogPublicKey, {
     api_host: runtimeConfig.public.posthogHost || 'https://app.posthog.com',
+    capture_pageview: false, // we do SPA routing ourselves
     loaded: (posthogInstance) => {
       if (import.meta.env.MODE === 'development') posthogInstance.debug();
     },
+  });
+
+  const router = useRouter();
+
+  const capturePageview = (path: string) => {
+    posthog.capture('$pageview', {
+      $current_url: window.location.origin + path,
+    });
+  };
+
+  // initial page load
+  capturePageview(router.currentRoute.value.fullPath);
+
+  // subsequent SPA navigations
+  router.afterEach((to) => {
+    capturePageview(to.fullPath);
   });
 
   return {
